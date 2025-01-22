8950174c7bec303b4f5b8fa0622d7fba94b4730d
ryutaroM <arcticmonkey326@gmail.com>
2025-01-23 00:14:01 +0900

    Add core lib and url struct

---
+++ saba_core/src/url.rs
@@ -0,0 +1,26 @@
+use alloc::string::{String, ToString};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Url {
+    url: String,
+    host: String,
+    port: String,
+    path: String,
+    searchpart: String,
+}
+
+impl Url {
+    pub fn new(url: String) -> Self {
+        Self {
+            url,
+            host: "".to_string(),
+            port: "".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<Self, String> {
+        // next section
+    }
+}
