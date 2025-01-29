5ef4fc34f1917bfaae1217aa164d8371b1a80abe
ryutaroM <arcticmonkey326@gmail.com>
2025-01-29 23:32:44 +0900

    add extract url parts method

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -1,4 +1,5 @@
 use alloc::string::{String, ToString};
+use alloc::vec::Vec;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Url {
@@ -21,6 +22,86 @@ impl Url {
     }
 
     pub fn parse(&mut self) -> Result<Self, String> {
-        // next section
+        if !self.is_http() {
+            return Err("Only HTTP scheme is supported".to_string());
+        }
+
+        self.host = self.extract_host();
+        self.port = self.extract_port();
+        self.path = self.extract_path();
+        self.searchpart = self.extract_searchpart();
+
+        Ok(self.clone())
+    }
+
+    fn is_http(&mut self) -> bool {
+        if self.url.contains("http://") {
+            return true;
+        }
+        false
+    }
+
+    fn extract_host(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][..index].to_string()
+        } else {
+            url_parts[0].to_string()
+        }
+    }
+
+    fn extract_port(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if let Some(index) = url_parts[0].find(":") {
+            url_parts[0][index + 1..].to_string()
+        } else {
+            "80".to_string()
+        }
+    }
+
+    fn extract_path(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+
+        path_and_searchpart[0].to_string()
+    }
+
+    fn extract_searchpart(&self) -> String {
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+
+        if path_and_searchpart.len() < 2 {
+            "".to_string()
+        } else {
+            path_and_searchpart[1].to_string()
+        }
     }
 }
