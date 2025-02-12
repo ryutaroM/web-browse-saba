6992b92c4eeead91375f4b3367b7df4b426d2fbd
ryutaroM <arcticmonkey326@gmail.com>
2025-02-13 00:22:37 +0900

    add lib net

---
+++ net/wasabi/src/http.rs
@@ -0,0 +1,34 @@
+extern crate alloc;
+use crate::alloc::string::ToString;
+use alloc::format;
+use alloc::string::String;
+use noli::net::lookup_host;
+use noli::net::SocketAddr;
+use saba_core::error::Error;
+use saba_core::http::HttpResponse;
+
+pub struct HttpClient {}
+
+impl HttpClient {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn get(&self, host: String, port: u16, path: String) -> Result<HttpResponse, Error> {
+        let ips = match lookup_host(&host) {
+            Ok(ips) => ips,
+            Err(e) => {
+                return Err(Error::Network(format!(
+                    "Failed to find IP address: {:#?}",
+                    e
+                )));
+            }
+        };
+
+        if ips.len() < 1 {
+            return Err(Error::Network("Failed to find IP addresses".to_string()));
+        }
+
+        let socket_addr: SocketAddr = (ips[0], port).into();
+    }
+}
