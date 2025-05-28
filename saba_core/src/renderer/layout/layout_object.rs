49e83e848989672b3ff71d507fe9fc3c38a50ebb
ryutaroM <arcticmonkey326@gmail.com>
2025-05-28 23:50:27 +0900

    add partialeq trait for layoutobject

--- saba_core/src/renderer/layout/layout_object.rs
+++ saba_core/src/renderer/layout/layout_object.rs
@@ -29,6 +29,12 @@ pub struct LayoutObject {
     size: LayoutSize,
 }
 
+impl PartialEq for LayoutObject {
+    fn eq(&self, other: &Self) -> bool {
+        self.kind == other.kind
+    }
+}
+
 impl LayoutObject {
     pub fn new(node: Rc<RefCell<Node>>, parent_obj: &Option<Rc<RefCell<LayoutObject>>>) -> Self {
         let parent = match parent_obj {
