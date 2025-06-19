571db76bd62339c2f8807e29e6b627a6f030e92e
ryutaroM <arcticmonkey326@gmail.com>
2025-06-19 23:19:15 +0900

    handle URL parsing and HTTP response handling in the browser

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,81 @@
 
 extern crate alloc;
 
+use crate::alloc::string::String;
 use crate::alloc::string::ToString;
+use alloc::fmt::format;
+use alloc::format;
 use alloc::rc::Rc;
 use core::cell::RefCell;
+use net_wasabi::http::HttpClient;
 use noli::*;
 use saba_core::browser::Browser;
+use saba_core::error::Error;
 use saba_core::http::HttpResponse;
+use saba_core::url::Url;
 use ui_wasabi::app::WasabiUI;
 
-static TEST_HTTP_RESPONSE: &str = r#"HTTP/1.1 200 OK
-Data: xx xx xx
+fn handle_url(url: String) -> Result<HttpResponse, Error> {
+    let parsed_url = match Url::new(url.to_string()).parse() {
+        Ok(url) => url,
+        Err(e) => {
+            return Err(Error::UnexpectedInput(format!(
+                "input html is not supported: {:?}",
+                e
+            )));
+        }
+    };
 
+    let client = HttpClient::new();
+    let response = match client.get(
+        parsed_url.host(),
+        parsed_url.port().parse::<u16>().expect(&format!(
+            "port number should be u16 but got {}",
+            parsed_url.port(),
+        )),
+        parsed_url.path(),
+    ) {
+        Ok(res) => {
+            if res.status_code() == 302 {
+                let location = match res.header_value("Location") {
+                    Ok(value) => value,
+                    Err(_) => return Ok(res),
+                };
+                let redirect_parsed_url = Url::new(location);
 
-<html>
-<head></head>
-<body>
-  <h1 id="title">H1 title</h1>
-  <h2 class="class">H2 title</h2>
-  <p>Test text.</p>
-  <p>
-    <a href="example.com">Link1</a>
-    <a href="example.com">Link2</a>
-  </p>
-</body>
-</html>
-"#;
+                let redirect_res = match client.get(
+                    redirect_parsed_url.host(),
+                    redirect_parsed_url.port().parse::<u16>().expect(&format!(
+                        "port number should be u16 but got {}",
+                        redirect_parsed_url.port(),
+                    )),
+                    redirect_parsed_url.path(),
+                ) {
+                    Ok(res) => res,
+                    Err(e) => return Err(Error::Network(format!("{:?}", e))),
+                };
+
+                redirect_res
+            } else {
+                res
+            }
+        }
+        Err(e) => {
+            return Err(Error::Network(format!(
+                "failed to get http response: {:?}",
+                e
+            )))
+        }
+    };
+    Ok(response)
+}
 
 fn main() -> u64 {
     let browser = Browser::new();
 
     let ui = Rc::new(RefCell::new(WasabiUI::new(browser)));
 
-    match ui.borrow_mut().start() {
+    match ui.borrow_mut().start(handle_url) {
         Ok(_) => {}
         Err(e) => {
             println!("browser fails to start {:?}", e);
--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -20,6 +20,7 @@ use saba_core::constants::WINDOW_INIT_Y_POS;
 use saba_core::constants::WINDOW_WIDTH;
 use saba_core::constants::*;
 use saba_core::error::Error;
+use saba_core::http::HttpResponse;
 
 #[derive(Debug)]
 pub struct WasabiUI {
@@ -102,18 +103,24 @@ impl WasabiUI {
         Ok(())
     }
 
-    pub fn start(&mut self) -> Result<(), Error> {
+    pub fn start(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
         self.setup()?;
 
-        self.run_app()?;
+        self.run_app(handle_url)?;
 
         Ok(())
     }
 
-    fn run_app(&mut self) -> Result<(), Error> {
+    fn run_app(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
         loop {
             self.handle_mouse_input()?;
-            self.handle_key_input()?;
+            self.handle_key_input(handle_url)?;
         }
     }
 
@@ -132,14 +139,22 @@ impl WasabiUI {
     //     Ok(())
     // }
 
-    fn handle_key_input(&mut self) -> Result<(), Error> {
+    fn handle_key_input(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+    ) -> Result<(), Error> {
         match self.input_mode {
             InputMode::Normal => {
                 let _ = Api::read_key();
             }
             InputMode::Editing => {
                 if let Some(c) = Api::read_key() {
-                    if c == 0x7F as char || c == 0x08 as char {
+                    if c == 0x0A as char {
+                        self.start_navigation(handle_url, self.input_url.clone())?;
+
+                        self.input_url = String::new();
+                        self.input_mode = InputMode::Normal;
+                    } else if c == 0x7F as char || c == 0x08 as char {
                         self.input_url.pop();
                         self.update_address_bar()?;
                     } else {
@@ -255,6 +270,47 @@ impl WasabiUI {
                 self.input_mode = InputMode::Normal;
             }
         }
+
+        Ok(())
+    }
+
+    fn start_navigation(
+        &mut self,
+        handle_url: fn(String) -> Result<HttpResponse, Error>,
+        destination: String,
+    ) -> Result<(), Error> {
+        self.clear_address_bar()?;
+
+        match handle_url(destination) {
+            Ok(response) => {
+                let page = self.browser.borrow().current_page();
+                page.borrow_mut().receive_response(response);
+            }
+            Err(e) => {
+                return Err(e);
+            }
+        }
+
+        Ok(())
+    }
+
+    fn clear_content_area(&mut self) -> Result<(), Error> {
+        if self
+            .window
+            .fill_rect(
+                WHITE,
+                0,
+                TOOLBAR_HEIGHT + 2,
+                CONTENT_AREA_WIDTH,
+                CONTENT_AREA_HEIGHT - 2,
+            )
+            .is_err()
+        {
+            return Err(Error::InvalidUI(
+                "failed to clear a content area".to_string(),
+            ));
+        }
+        self.window.flush();
 
         Ok(())
     }
