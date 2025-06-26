70c3a584e30b6ffb8bd10b1381d662dcffce02c4
ryutaroM <arcticmonkey326@gmail.com>
2025-06-26 22:37:41 +0900

    add JavaScript lexer and tokenization functionality

---
+++ saba_core/src/renderer/js/token.rs
@@ -0,0 +1,77 @@
+use alloc::string::String;
+use alloc::string::ToString;
+use alloc::vec::Vec;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Token {
+    Punctuator(char),
+    Number(u64),
+}
+
+pub struct JsLexer {
+    pos: usize,
+    input: Vec<char>,
+}
+
+impl JsLexer {
+    pub fn new(js: String) -> Self {
+        Self {
+            pos: 0,
+            input: js.chars().collect(),
+        }
+    }
+
+    fn consume_number(&mut self) -> u64 {
+        let mut num = 0;
+
+        loop {
+            if self.pos >= self.input.len() {
+                return num;
+            }
+
+            let c = self.input[self.pos];
+
+            match c {
+                '0'..='9' => {
+                    num = num * 10 + (c.to_digit(10).unwrap() as u64);
+                    self.pos += 1;
+                }
+                _ => break,
+            }
+        }
+
+        return num;
+    }
+}
+
+impl Iterator for JsLexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        while self.input[self.pos] == ' ' || self.input[self.pos] == '\n' {
+            self.pos += 1;
+
+            if self.pos >= self.input.len() {
+                return None;
+            }
+        }
+
+        let c = self.input[self.pos];
+
+        let token = match c {
+            '+' | '-' | ';' | '=' | '(' | ')' | '{' | '}' | ',' | '.' => {
+                let t = Token::Punctuator(c);
+                self.pos += 1;
+                t
+            }
+            '0'..='9' => Token::Number(self.consume_number()),
+            _ => unimplemented!("char {:?} is not supported yet", c),
+        };
+
+        Some(token)
+    }
+}
